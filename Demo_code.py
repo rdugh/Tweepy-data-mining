557c43863721fca1f09f24584d093c98ebfe4529
Liwei Wang <livi@liweis-mbp.lan>
2020-10-10 11:34:55 -0400

    Successfully saved the data

--- Demo_code.py
+++ Demo_code.py
@@ -1,15 +1,21 @@
 import tweepy
 import Twitter_credential
+import numpy as np
+import pandas as pd
 
-
+'''
+class TwitterAuthenticator():
+    def authenticate_twitter_app(self):'''
+        
 #creating a stream listener
 class MyStreamListener(tweepy.StreamListener):
     '''Basic listener class that just print received tweets to stdout.'''
     def __init__(self, fetched_tweets_filename):
         self.fetched_tweets_filename = fetched_tweets_filename
+        self.num_tweets=0
 
     def on_data(self,data):
-        #print(data)
+        print(data)
         try:
             with open(self.fetched_tweets_filename,'a') as tf:
                 tf.write(data)
@@ -19,7 +25,14 @@ class MyStreamListener(tweepy.StreamListener):
             return True
 
     def on_status(self, status):
-        print(status.text)
+        record={'Text':status.text,'Created At':status.created_at}
+        print(record)
+        self.num_tweets+=1
+        if self.num_tweets<5:
+            #collection.insert(record)
+            return True
+        else:
+            return False
 
     def on_error(self, status_code):
         print(status_code)
@@ -36,7 +49,6 @@ class TwitterStreamer():
         pass
     def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
         #This handles Twitter authentication and the connection to the Twitter Streaming API.
-        
         #Authentication 
         auth = tweepy.OAuthHandler(Twitter_credential.API_key, Twitter_credential.API_secret_key)# responsibel authenticating our code
         auth.set_access_token(Twitter_credential.Access_token, Twitter_credential.Access_token_secret)
@@ -53,7 +65,7 @@ class TwitterStreamer():
 
 
 if __name__=='__main__':
-    hash_tag_list=['SDG']
-    fetched_tweets_filename='tweets.json'
+    hash_tag_list=['Trump']
+    fetched_tweets_filename='Testtweets.json'
     twitter_streamer=TwitterStreamer()
     twitter_streamer.stream_tweets(fetched_tweets_filename,hash_tag_list)
